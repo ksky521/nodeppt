70c083182ed0a48aa4ab8094b3de17d12ed15312
ksky521 <ksky521@gmail.com>
2013-03-17 18:54:18 +0800

    修改postMessage的proxyFn

--- src/js/nodeppt.control.postMessage.js
+++ src/js/nodeppt.control.postMessage.js
@@ -1,6 +1,6 @@
 /**
  * postMessage 只能单方面控制
- * 
+ *
  */
 Slide.Control.add('postMessage', function(S, broadcast) {
 
@@ -10,32 +10,55 @@ Slide.Control.add('postMessage', function(S, broadcast) {
 		update: function(id) {
 			if (postWin) {
 				window.opener.postMessage({
-					action:'update',
+					action: 'update',
 					id: id
 				}, '*');
-			} 
+			}
 
 		},
 		updateItem: function(id, item) {
 			if (postWin) {
 				window.opener.postMessage({
-					action:'updateItem',
+					action: 'updateItem',
 					id: id,
 					item: item
-				},'*');
+				}, '*');
 			}
 
+		},
+		keyEvent: function(keyCode) {
+			if (postWin) {
+				window.opener.postMessage({
+					action: 'keyEvent',
+					keyCode: keyCode
+				}, '*');
+			}
 		},
 		// evtControl: function(e) {
 		// 	console.log('client 发来贺电', arguments);
 		// },
 		evtClient: function(e) {
 			var data = e.data;
-			if(data.action==='update'){
-				broadcast.fire('from control update', data.id);
-			}else{
-				broadcast.fire('from control updateItem', data.id, data.item);
+			switch (data.action) {
+				case 'update':
+					broadcast.fire('from control update', data.id);
+					break;
+				case 'updateItem':
+					broadcast.fire('from control updateItem', data.id, data.item);
+					break;
+				case 'keyEvent':
+					broadcast.fire('from control key event', data.keyCode);
+					break;
+				case 'userOrder':
+					var fnName = data.fn;
+					var args = data.args;
+					try {
+						args = JSON.parse(args);
+					} catch (e) {}
+					Slide.proxyFn(fnName, args);
+					break;
 			}
+
 		},
 		init: function(args) {
 			var t = this;
@@ -43,11 +66,20 @@ Slide.Control.add('postMessage', function(S, broadcast) {
 				this.role = 'client';
 				var url = location.href.split('#')[0] + '#control';
 				var temp = 'height=300, width=400, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, location=no, status=yes';
-				postWin = window.open(url, 'ppt', temp);
+				window.open(url, 'ppt', temp);
 				window.addEventListener('message', this.evtClient, false);
-				
+
 			} else {
 				this.role = 'control';
+				//如果是控制端，则重写proxyFn函数
+				Slide.proxyFn = function(fnName, args) {
+					args = JSON.stringify(args);
+					window.opener.postMessage({
+						action: 'userOrder',
+						fn: fnName,
+						args: args
+					}, '*');
+				}
 				document.body.classList.add('popup');
 				document.body.classList.add('with-notes');
 				postWin = window.opener;
