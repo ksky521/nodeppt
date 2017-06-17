56afb489d1d295a6670d9af2a11f87c3a62165ba
ksky521 <ksky521@gmail.com>
2017-06-17 10:37:12 +0800

    增加gh-pages 生成脚本

---
+++ gh-page.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+rm -rf publish
+node ./bin/nodeppt release ppts/demo.md -a
+
+cd publish
+mv demo.html index.html
+
+git init
+git add -A
+date_str=`date "+DATE: %m/%d/%Y%nTIME: %H:%M:%S"`
+git commit -m "build with nodeppt on $date_str"
+exit
+echo 'push remote github'
+git push -u git@github.com:ksky521/nodeppt.git master:gh-pages --force
